50fc1ec09953f03b3b5893b41c0b87c0e81e29b0
Josh <semptimus22@gmail.com>
2023-07-16 16:29:18 -0400

    Add project files.

---
+++ Discorse/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Discorse.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discorse.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                isNativeOrigin = false,
+                FirstMessage = true
+            });
+            
+            for (int i = 0; i < 4; i++) 
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    isNativeOrigin = true,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Bunny",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Last",
+                    Time = DateTime.Now,
+                    isNativeOrigin = false,
+                });
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                }); ;
+            }
+        }
+    }
+}
